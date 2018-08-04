6835c24d12390fd358cb28e36ffd5f177eb1d28c
unknown <natorvano.work@gmail.com>
2018-08-04 14:29:21 +0300

    olx adv. logic story

--- src/sandbox/olxRedirect.ts
+++ src/sandbox/olxRedirect.ts
@@ -1,9 +1,9 @@
 import { fetchFactory } from "../factories/network/utils/fetchFactory";
 
-export const run = () =>
-  fetchFactory("https://www.olx.ua/list/q-1060-6gb/?page=70").then(response => {
-    response.data = null;
-    // tslint:disable-next-line
-    console.log(response);
-    // https://www.olx.ua/list/?page=17
-  });
+fetchFactory("https://www.olx.ua/list/q-1060-6gb/?page=70").then(response => {
+  response.data = null;
+  // tslint:disable-next-line
+  console.log(response);
+  // data = null, status: 301 (Actually, the status is 200, but it is determined
+  // programmatically)
+});
